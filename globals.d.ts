1c7ce19a5bf6b1c215f5d2d08adb254c0bb737b0
Binyamin Galinsky <binyamin@lightricks.com>
2019-02-25 14:36:28 +0200

    index.js: clean javascript out of index.html

---
+++ globals.d.ts
@@ -0,0 +1,6 @@
+declare function stringToUTF8(id: any, idBuffer: any, length: number);
+declare function ccallArrays(name: string, item: any, type: string[], imageData: Uint8ClampedArray[], userData: Object<string, any>)
+
+declare const Module {
+
+}
